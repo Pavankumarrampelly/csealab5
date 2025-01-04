3c5cfe70aca555f2ae0bc7e418b39c9312a176a2
Pavankumarrampelly <rampellypavankumar2003@gmail.com>
2025-01-04 11:18:17 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -41,3 +41,41 @@ CMD ["node", "app.js"]*/
  }
  }
 }*/
+//java Jenkins 
+/*pipeline {
+    agent any
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch: 'main', url: 'https://github.com/deepak574/project.git'
+            }
+        }
+        stage('Build') {
+            steps {
+             
+                bat 'javac Sample.java'
+            }
+        }
+        stage('Test') {
+            steps {
+                             bat 'java Sample'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying application...'
+                // Add your deployment commands here (like copying files to a server)
+            }
+        }
+post
+{
+success 
+{
+mail to:’gujjuladeepak@gmail.com’,
+subject: “Build success”,
+Body: ‘Your build was successfull’
+}
+}
+    }
+}*/
