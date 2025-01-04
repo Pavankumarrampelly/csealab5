84f8c0c10de5ea4aa0f7e3b6090ba72ef58af0c4
Pavankumarrampelly <rampellypavankumar2003@gmail.com>
2025-01-04 12:25:52 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -90,3 +90,12 @@ COPY . .
 RUN javac random.java
 # Command to run the program
 CMD ["java", "random"]*/
+//node program 
+/*const express = require('express');
+const app = express();
+app.get('/',(req,res) => {
+res.send("Hello World, I am from Nodejs");
+});
+app.listen(3020,() => {
+console.log("Server is running on port 3020")
+});*/
