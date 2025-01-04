a9eb126033633b2165a5cf9c70abe0f6855b9859
Pavankumarrampelly <rampellypavankumar2003@gmail.com>
2025-01-04 11:46:41 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -79,3 +79,14 @@ Body: ‘Your build was successfull’
 }
     }
 }*/
+//java docker
+/*# Use an official OpenJDK runtime as a parent image
+FROM openjdk:11-jdk-slim
+# Set the working directory inside the container
+WORKDIR /app
+# Copy the current directory contents into the container at /app
+COPY . .
+# Compile the Java program
+RUN javac random.java
+# Command to run the program
+CMD ["java", "random"]*/
